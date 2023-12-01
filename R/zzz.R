27f21d99d7137afaa6d823d644a41207dba04cdf
Tony <66729956+tony-aw@users.noreply.github.com>
2023-12-01 11:52:41 +0100

    Delete R directory

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  txt <- paste0(
-    "Run `",
-    '?subsets::subsets',
-    "` to open the introduction help page of 'subsets'."
-  )
-  packageStartupMessage(txt)
-}
